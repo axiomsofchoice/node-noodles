1783f1b20f72bc1323a5a7c2a964720fcb6b990d
Dan Hagon <axiomsofchoice@gmail.com>
2011-08-28 19:30:48 +0100

    Removed an additional open that\'s not now needed.

--- npmjs-api.js
+++ npmjs-api.js
@@ -26,43 +26,35 @@ exports.npmjsCronJob = function (db) {
             // First sort the package list to make it easier to detect changes
             var package_list = data.sort() ;
             
-            // Connect to the MongoDB server and check for changes
-            db.open(function(err, db) {
-              if(!err) {
+            // Get the packages collection
+              db.collection('node-packages', function(err, collection) {
                   
-                  console.log("Connected to MongoDB server.");
-                  
-                  db.collection('node-packages', function(err, collection) {
-                      
-                      // Get current list of packages, which is a special doc
-                      collection.findOne({_id:"4e5a85d99643f10007000005"}, 
-                        function(err, current_package_list) {
-                              
-                              // FOR TESTING ONLY!!
-                              var packageName = 
-                                current_package_list.package_list[0] ;
-                              
-                              console.log('New package found: ' + packageName) ;
-                              
-                              // Get the metadata for this new package and
-                              // insert into the db
-                              var full_request_url = repo_url + '?' 
-                                        + querystring.stringify( packageName );
-                              
-                              rest.get(full_request_url).on('complete',
-                                function(data) {
-                                  collection.insert(data, {safe:true},
-                                    function(err, result) {
-                                            console.log("Failed to insert new" +
-                                                "package metadata for: "
-                                                + packageName);
-                                   });
-                              });
-                        });
-                  });
-              } else {
-                  console.log("Failed to connect to MongoDB server.");
-              }
+                  // Get current list of packages, which is a special doc
+                  collection.findOne({_id:"4e5a85d99643f10007000005"}, 
+                    function(err, current_package_list) {
+                          
+                          // FOR TESTING ONLY!!
+                          var packageName = 
+                            current_package_list.package_list[0] ;
+                          
+                          console.log('New package found: ' + packageName) ;
+                          
+                          // Get the metadata for this new package and
+                          // insert into the db
+                          var full_request_url = repo_url + '?' 
+                                    + querystring.stringify( packageName );
+                          
+                          rest.get(full_request_url).on('complete',
+                            function(data) {
+                              collection.insert(data, {safe:true},
+                                function(err, result) {
+                                        console.log("Failed to insert new" +
+                                            "package metadata for: "
+                                            + packageName);
+                               });
+                          });
+                    });
+                }
             });
             
     });
