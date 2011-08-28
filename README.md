9d9b9c26c7b82692203e21e84da7c57d62f932f7
Dan Hagon <axiomsofchoice@gmail.com>
2011-08-28 17:47:42 +0100

    Documented the project in the source code and README.

---
+++ README.md
@@ -0,0 +1,6 @@
+npmviz
+======
+
+# This is an entry in the 2011 Node Knockout for team Node Noodles.
+
+The app displays a graph visualization of the dependencies between packages found in the main NPM package registry at npmjs.org. Uses the Express framework with Jade page templates. On the backend it uses MongoDB, which for the duration of the competition was hosted on MongoHQ. JSON REST API calls are made with restler.
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 /**
+*  The main server that runs the npmviz app.
 *  
-*
-*
+*  This is an entry in the 2011 Node Knockout for team Node Noodles.
+*  
+*  The app displays a graph visualization of the dependencies between packages
+*  found in the main NPM package registry at npmjs.org. Uses the Express
+*  framework with Jade page templates. On the backend it uses MongoDB, which for
+*  the duration of the competition was hosted on MongoHQ. JSON REST API calls
+*  are made with restler.
+*  
+*  @author axiomsofchoice
 */
 
 var http = require('http')
