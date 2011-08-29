cd5be6516ebbb234a74e418e0946135e9912b36e
Dan Hagon <axiomsofchoice@gmail.com>
2011-08-29 01:23:08 +0100

    Fixing stuff

--- npmjs-api.js
+++ npmjs-api.js
@@ -101,7 +101,7 @@ exports.npmjsGetPackageDetails = function (cb) {
                   
                   current_package_list['package_list'].forEach( function(item) {
                           packages.push({"name":item,"group":1});
-                  };
+		      });
     var myJson = {"nodes":packages,
                     "links":[{"source":1,"target":0,"value":1},{"source":2,"target":0,"value":8},{"source":3,"target":0,"value":10},{"source":3,"target":2,"value":6},{"source":4,"target":0,"value":1},{"source":5,"target":0,"value":1},{"source":6,"target":0,"value":1},{"source":7,"target":0,"value":1},{"source":8,"target":0,"value":2},{"source":9,"target":0,"value":1},{"source":11,"target":10,"value":1},{"source":11,"target":3,"value":3},{"source":11,"target":2,"value":3},{"source":11,"target":0,"value":5},{"source":12,"target":11,"value":1},{"source":13,"target":11,"value":1},{"source":14,"target":11,"value":1},{"source":15,"target":11,"value":1},{"source":17,"target":16,"value":4},{"source":18,"target":16,"value":4},{"source":18,"target":17,"value":4},{"source":19,"target":16,"value":4},]} ;
     
